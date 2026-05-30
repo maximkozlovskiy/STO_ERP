f2a295460ad35a91557e6ca4dab516fbaea067c6
Максим Козловський <your@email.com>
2026-05-30 22:23:10 +0300

    feat(dx): sprint-A2 — ESLint react-hooks/rules-of-hooks + exhaustive-deps

--- packages/config/eslint/nextjs.js
+++ packages/config/eslint/nextjs.js
@@ -1,7 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['./base.js', 'next/core-web-vitals'],
+  extends: ['./base.js', 'next/core-web-vitals', 'plugin:react-hooks/recommended'],
+  plugins: ['react-hooks'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
